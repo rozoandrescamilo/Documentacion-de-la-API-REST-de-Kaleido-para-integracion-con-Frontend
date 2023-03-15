db491510e17a97aa869d2447e1ff34955e017f10
Andrés Rozo <80934853+rozoandrescamilo@users.noreply.github.com>
2023-03-15 07:24:49 -0500

    Update StatusValidator.sol

--- StatusValidator.sol
+++ StatusValidator.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract StatusValidator {
+
     // Define una estructura de datos llamada Step para almacenar información sobre un paso en el flujo de proceso
     struct Step {
         Status status; // El estado actual del paso
@@ -10,52 +11,81 @@ contract StatusValidator {
 
     // Un enumerable llamado Status que define los diferentes estados posibles de un paso en el flujo de proceso
     enum Status {
-        CONFIRMED_PO, // status 0
-        BOOKING_REQUEST, // status 1
-        SHIPMENT_IN_TRANSIT, // status 2
-        SHIPMENT_AT_DESTINATION, // status 3
-        CUSTOMS, // status 4
-        DELIVERED, // status 5
-        COMPLETED // status 6
-    }
+        TO_BE_CONFIRMED, 
+        APPROVED_P1, 
+        TO_BE_CONFIRMED_P2, 
+        APPROVED_P2, 
+        TO_BE_CONFIRMED_P3, 
+        APPROVED_P3,
+        BOOKING_REQUEST 
+    } 
 
     // Un evento llamado RegisteredStep que se activa cuando una PO se registra en el flujo 
     event RegisteredStep(
         uint256 POID, // ID del producto
+        uint256 poType, // Tipo de orden: Entrega total, 2 parciales o 3 parciales
         Status status, // Estado del paso
         string metadata, // Metadatos adicionales
         address author // Dirección del autor
     );
 
     // El mapping guarda el ID de la PO y un array asociado con cada Step de la misma.
-    mapping(uint256 => Step[]) public POvalidator;
+    mapping(uint256 => Step[]) public ParameterValidator;
 
     // Una función para registrar una PO que contendrá steps.
-    function registerPO(uint256 POID) public returns (bool success) {
+    function RegisterPO(address userWallet, uint256 POID) public returns (bool success) {
+        // Comprueba si la dirección de la wallet del usuario es igual a la del usuario que intenta interactuar
+        require(userWallet == msg.sender, "To be able to interact with your Purchase Order you must use your registered wallet address");
         // Comprueba que la PO no haya sido registrado previamente.
-        require(POvalidator[POID].length == 0, "This product already exists");
-        // Agrega un paso inicial al producto con el estado "CONFIRMED_PO".
-        POvalidator[POID].push(Step(Status.CONFIRMED_PO, ""));
+        require(ParameterValidator[POID].length == 0, "This product already exists");
+        // Agrega un paso inicial al producto con el estado "TO_BE_CONFIRMED".
+        ParameterValidator[POID].push(Step(Status.TO_BE_CONFIRMED, ""));        
         return success;
+
     }
 
-    // Una función para registrar un nuevo paso en el flujodel proceso.
-    function registerStep(uint256 POID, string calldata metadata) public returns (bool success){
+    mapping(address => bool) allowedWallets;
+
+    constructor() {
+        allowedWallets[0x5B38Da6a701c568545dCfcB03FcB875f56beddC4] = true; // agregar direccion de wallet permitida
+        allowedWallets[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] = true; // agregar direccion de wallet permitida
+        allowedWallets[0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db] = true; // agregar otra direccion de wallet permitida
+    }
+
+    // Una función para registrar un nuevo paso en el flujo del proceso.
+    function RegisterStep(address userWallet, uint256 POID, string calldata metadata, uint256 poType) public returns (bool success) {
+        // Comprueba si la wallet esta entre las permitidas y si la dirección de la wallet del usuario es igual a la del usuario que intenta interactuar
+        require(allowedWallets[userWallet] && userWallet == msg.sender, "To be able to interact with your Purchase Order you must use your registered wallet address");
         // Comprueba que la PO haya sido registrado previamente.
-        require(POvalidator[POID].length > 0, "This Purchase Order doesn't exist");
+        require(ParameterValidator[POID].length > 0, "This Purchase Order doesn't exist");
+        // Comprueba que los tipos orden sean los correctos.
+        require(poType == 1 || poType == 2 || poType == 3, "Invalid PO type");
         // Obtiene la matriz de pasos actual para el producto.
-        Step[] memory stepsArray = POvalidator[POID];
+        Step[] memory stepsArray = ParameterValidator[POID];
         // Calcula el estado siguiente para el paso actual.
         uint256 currentStatus = uint256(stepsArray[stepsArray.length - 1].status) + 1;
         // Dado el caso de que el status sea mayor a COMPLETED envía error
-        if (currentStatus > uint256(Status.COMPLETED)) {
+        if (currentStatus > uint256(Status.BOOKING_REQUEST)) {
             revert("The Purchase Order has no more steps");
+        }
+        // Dado el tipo de estado 1 se da el máximo estado posible 2
+        if (poType == 1 && currentStatus > 2) {
+            revert("The maximum status for poType 1 is 2 BOOKING REQUEST");
+        }
+        // Dado el tipo de estado 2 se da el máximo estado posible 4
+        if (poType == 2 && currentStatus > 4) {
+            revert("The maximum status for poType 2 is 4 BOOKING REQUEST");
+        }
+        // Dado el tipo de estado 3 se da el máximo estado posible 6
+        if (poType == 3 && currentStatus > 6) {
+            revert("The maximum status for poType 3 is 6 BOOKING REQUEST");
         }
         // Se asigna el estado actual + 1 y se añade al mapping de PO
         Step memory step = Step(Status(currentStatus), metadata);
-        POvalidator[POID].push(step);
+        ParameterValidator[POID].push(step);
         // Se lanza evento donde se registra nuevo step a unu PO
-        emit RegisteredStep(POID, Status(currentStatus), metadata, msg.sender);
+        emit RegisteredStep(POID, poType, Status(currentStatus), metadata, msg.sender);
         success = true;
     }
+
 }
